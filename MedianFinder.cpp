6cb205f4e80731315768a721afc32fd83733b623
Raja kumar <168344895+rajagceian@users.noreply.github.com>
2025-04-26 00:24:47 +0530

    Leetcode 295

---
+++ MedianFinder.cpp
@@ -0,0 +1,34 @@
+class MedianFinder {
+public:
+    priority_queue<int> maxHeap; // Left half (max-heap)
+    priority_queue<int, vector<int>, greater<int>>
+        minHeap; // Right half (min-heap)
+
+    MedianFinder() {}
+
+    void addNum(int num) {
+        if (maxHeap.empty() || num <= maxHeap.top()) {
+            maxHeap.push(num);
+        } else {
+            minHeap.push(num);
+        }
+
+        // Rebalance the heaps
+        if (maxHeap.size() > minHeap.size() + 1) {
+            minHeap.push(maxHeap.top());
+            maxHeap.pop();
+        } else if (minHeap.size() > maxHeap.size()) {
+            maxHeap.push(minHeap.top());
+            minHeap.pop();
+        }
+    }
+
+    double findMedian() {
+        if (maxHeap.size() == minHeap.size()) {
+            return (maxHeap.top() + minHeap.top()) / 2.0;
+        } else {
+            return maxHeap
+                .top(); // Always maxHeap will have more elements if odd
+        }
+    }
+};
